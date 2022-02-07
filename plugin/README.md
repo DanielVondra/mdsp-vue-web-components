7c442535becc14f975d5b4838ebd86ea458f0b85
DanielVondra <daniel.vondra@vdttechnoligy.com>
2022-02-07 15:47:28 +0100

    no script required -- we handle it 👍

--- plugin/README.md
+++ plugin/README.md
@@ -1,9 +1,1 @@
 # Vue 3 wrapper for MindSphere Web Components
-
-
-You will need to add script tag to your head as follows. 👇
-```html
-<script id="mdsp-webcomponents-loader" type="text/javascript"
-  src="https://static.eu1.mindsphere.io/webcomponents/v1/index.js" data-onload="mdspwc_loaded"></script>
-<script>function mdspwc_loaded() { console.log('MdspWebComponents loaded ✔️') };</script>
-```
--- plugin/src/index.js
+++ plugin/src/index.js
@@ -1,9 +1,20 @@
 import components from './components'
-
+    
 const plugin = {
   install: (app, options) => {
     for (const key in components) {
       app.component(key, components[key])
+    }
+    const _el = document.getElementById('mdsp-webcomponents-loader');
+    if (!_el) {
+      let mdsp_script = document.createElement('script')
+      mdsp_script.setAttribute('id', 'mdsp-webcomponents-loader')
+      mdsp_script.setAttribute('type', 'text/javascript')
+      mdsp_script.setAttribute('src', 'https://static.eu1.mindsphere.io/webcomponents/v1/index.js')
+      document.head.appendChild(mdsp_script)
+      console.log('MdspWebComponents loaded ✔️ - added');
+    } else {
+      console.log('MdspWebComponents loaded ✔️ - initial')
     }
   }
 }
